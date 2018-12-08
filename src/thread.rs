769d9ee71a883802b3c938060638cc3759d500c1
Newton Ni <nwtnni@gmail.com>
2018-12-08 04:32:17 -0500

    Finally figure out concrete types for tokio-serde wrappers

--- src/thread.rs
+++ src/thread.rs
@@ -5,7 +5,20 @@ pub(crate) mod peer;
 pub(crate) mod replica;
 pub(crate) mod scout;
 
+use futures::{sink, stream};
 use futures::sync::mpsc;
+use tokio::{io, net};
+use tokio::codec::{FramedRead, FramedWrite, LengthDelimitedCodec};
+use tokio_serde_bincode::{
+    ReadBincode as RB,
+    WriteBincode as WB,
+};
 
 pub type Tx<T> = mpsc::UnboundedSender<T>;
 pub type Rx<T> = mpsc::UnboundedReceiver<T>;
+
+type ReadTcp = io::ReadHalf<net::TcpStream>;
+type WriteTcp = io::WriteHalf<net::TcpStream>;
+
+pub type SocketRx<T> = RB<stream::FromErr<FramedRead<ReadTcp, LengthDelimitedCodec>, bincode::Error>, T>;
+pub type SocketTx<T> = WB<sink::SinkFromErr<FramedWrite<WriteTcp, LengthDelimitedCodec>, bincode::Error>, T>;
--- src/thread/replica.rs
+++ src/thread/replica.rs
@@ -1,6 +1,35 @@
+use std::marker;
+
+use futures::{stream, sink};
+use tokio_async_await::stream::StreamExt;
+use tokio_serde_bincode::{ReadBincode, WriteBincode};
+use tokio_serde_json::{ReadJson, WriteJson};
+use tokio::codec::{FramedRead, LengthDelimitedCodec};
+use tokio::net;
+
 use crate::message;
+use crate::state;
+use crate::thread::*;
 
 pub enum In<O> {
     Propose(message::Proposal<O>),
     Decide(message::Proposal<O>),
+}
+
+pub struct Replica<O, R, S> {
+    client_rx: SocketRx<O>,
+    client_tx: SocketTx<O>,
+    state: S,
+    _phantom: marker::PhantomData<R>,
+}
+
+impl<O: state::Operation + marker::Unpin, R: state::Response, S: state::State<O, R>> Replica<O, R, S> {
+    pub async fn run(mut self) {
+        loop {
+            while let Some(Ok(message)) = await!(self.client_rx.next()) {
+
+
+            }
+        }
+    }
 }
