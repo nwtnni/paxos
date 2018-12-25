88a4a4e597eb7ed0c91532a38cee167c8f3dec9a
Newton Ni <nwtnni@gmail.com>
2018-12-24 21:20:04 -0500

    Add documentation for storage module

--- paxos/src/storage.rs
+++ paxos/src/storage.rs
@@ -1,3 +1,13 @@
+//! # Summary
+//!
+//! This module abstracts over stable storage. To perform failure recovery, Paxos
+//! requires that some state persist between failures.
+//!
+//! Currently uses `bincode` to serialize the necessary state to the filesystem,
+//! and to deserialize it when recovering a process. This is a naive, inefficient
+//! implementation that clears the file on every write and re-serializes data
+//! from scratch.
+
 use std::io::Seek;
 
 /// Persistent storage for failure recovery.
@@ -7,6 +17,7 @@ pub struct Storage<S> {
 }
 
 impl<S> Storage<S> {
+    /// Creates a new stable storage file at relative path `path`.
     pub fn new<P: AsRef<std::path::Path>>(path: P) -> Self {
         let storage = std::fs::OpenOptions::new()
             .read(true)
@@ -22,12 +33,15 @@ impl<S> Storage<S> {
 }
 
 impl<S: serde::de::DeserializeOwned> Storage<S> {
+    /// Attempts to load state from disk, returning None if nothing
+    /// has been written or an error was encountered during deserialization.
     pub fn load(&self) -> Option<S> {
         bincode::deserialize_from(&self.storage).ok()
     }
 }
 
 impl<S: serde::Serialize> Storage<S> {
+    /// Saves state to disk. Will panic if modifying the underlying file fails.
     pub fn save(&mut self, state: &S) {
         self.storage.set_len(0)
             .expect("[STORAGE ERROR]: failed to trim file");
