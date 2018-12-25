28e24bfc26a15697b73b70f9509fa3221f9b7b59
Newton Ni <nwtnni@gmail.com>
2018-12-25 00:46:34 -0500

    Add documentation for replica

--- paxos/src/thread/replica.rs
+++ paxos/src/thread/replica.rs
@@ -1,3 +1,9 @@
+//! # Summary
+//!
+//! This module defines the `Replica` struct, which is responsible
+//! for communicating with the client and executing decisions on
+//! the state machine.
+
 use std::collections::HashMap as Map;
 
 use serde_derive::{Serialize, Deserialize};
@@ -10,18 +16,33 @@ use crate::state::Command;
 use crate::storage;
 use crate::thread::*;
 
+/// Replicas can only receive requests from the client,
+/// or decisions from commanders.
 #[derive(Debug)]
 pub enum In<C: state::Command> {
     Request(C),
     Decision(message::Proposal<C>),
 }
 
+/// Replicas maintain the actual state machine, and communicate
+/// with the client.
 pub struct Replica<S: state::State> {
+    /// Intra-server receiving channel
     rx: Rx<In<S::Command>>,
+
+    /// Intra-server leader transmitting channel
     leader_tx: Tx<leader::In<S::Command>>,
+
+    /// Intra-server shared transmitting channels
     shared_tx: shared::Shared<S>,
+
+    /// Persistent replica state across failures
     stable: Stable<S>,
+
+    /// Backing store for stable storage
     storage: storage::Storage<Stable<S>>,
+
+    /// User-provided state machine
     state: S,
 }
 
@@ -30,9 +51,16 @@ pub struct Replica<S: state::State> {
 #[derive(Derivative)]
 #[derivative(Default(bound = ""))]
 struct Stable<S: state::State> {
+    /// Slot for next proposal
     proposal_slot: usize,
+
+    /// Slot for next decision
     decision_slot: usize,
+
+    /// Map of latest proposals for each slot
     proposals: Map<usize, message::Command<S::Command>>,
+
+    /// Map of decisions for each slot
     decisions: Map<usize, message::Command<S::Command>>,
 }
 
@@ -63,10 +91,12 @@ impl<S: state::State> Replica<S> {
         }
     }
 
+    /// Propose the provided command.
     fn respond_request(&mut self, command: S::Command) {
         self.propose(command.into());
     }
 
+    /// Execute the provided decision, re-proposing any invalidated proposals.
     fn respond_decision(&mut self, decision: message::Proposal<S::Command>) {
         self.stable.decisions.insert(decision.s_id, decision.command);
         self.storage.save(&self.stable);
@@ -80,6 +110,7 @@ impl<S: state::State> Replica<S> {
         }
     }
 
+    /// Propose the provided command by delegating to the leader.
     fn propose(&mut self, command: message::Command<S::Command>) {
         for previous in self.stable.decisions.values() {
             if *previous == command { return }
@@ -103,6 +134,8 @@ impl<S: state::State> Replica<S> {
             .expect("[INTERNAL ERROR]: failed to send proposal");
     }
 
+    /// Perform the provided command by executing it on the state machine, sending
+    /// a response back to the client if there was one.
     fn perform(&mut self, command: message::Command<S::Command>) {
         for (s, previous) in &self.stable.decisions {
             if *previous == command && *s < self.stable.decision_slot {
